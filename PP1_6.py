997e43650acc5b1a0607e9447c91c35a7e8d4700
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 09:19:35 -0500

    Update PP1_6.py

--- PP1_6.py
+++ PP1_6.py
@@ -7,16 +7,16 @@
 
 # From input, recieve two integers from the user and add them together.  Output the result.
 def q1():
-  num1 = eval(input("Input a number: "))
-  num2 = eval(input("Input a number: "))
+  num1 = int(input("Input a number: "))
+  num2 = int(input("Input a number: "))
   
   print(num1+num2)
 
 # From input recieve two integers.  Output the quotient rounded down.
 
 def q2():
-  num1 = eval(input("Input a number: "))
-  num2 = eval(input("Input a number: "))
+  num1 = int(input("Input a number: "))
+  num2 = int(input("Input a number: "))
   
   print(round(num1/num2))
 
